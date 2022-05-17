b028bd928bcf11a3bccc3a0e781e794b0a4815de
unknown <mdrakibulhoque83@gmail.com>
2022-05-17 11:16:10 +0600

    practice files

---
+++ httpFun.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const http = require('http');
+
+const server = http.createServer( (req, res) => {
+   fs.readFile('./index.html', (err, data) => {
+       res.write(data);
+       res.end();
+   })
+})
+
+server.listen(5000, ()=>{
+    console.log('listening on port 5000')
+})
